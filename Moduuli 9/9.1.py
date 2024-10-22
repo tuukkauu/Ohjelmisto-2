11b7e50c6994778b475d8b8424aa391251ea1c78
tuukka <tuukkauu@metropolia.fi>
2024-10-22 22:51:57 +0300

    Tehtävä 9.1 ratkaistuna

--- Moduuli 9/9.1.py
+++ Moduuli 9/9.1.py
@@ -1,15 +1,14 @@
 class Auto:
-    def __init__(self, rekisteritunnus, huippunopeus, tamanhetkinen_nopeus = 0, kuljettu_matka = 0):
+    def __init__(self, rekisteritunnus, huippunopeus):
         self.rekisteritunnus = rekisteritunnus
         self.huippunopeus = huippunopeus
-        self.tamanhetkinen_nopeus = tamanhetkinen_nopeus
-        self.kuljettu_matka = kuljettu_matka
+        self.tamanhetkinen_nopeus = 0
+        self.kuljettu_matka = 0
 
-
+# auto-olio
 auto1 = Auto("ABC-123", 142)
 
 
 print(f"Auton rekisteritunnus on {auto1.rekisteritunnus:s}, huippunopeus on {auto1.huippunopeus:d}, "
       f"tämänhetkinen nopeus on {auto1.tamanhetkinen_nopeus:d} km/h ja kuljettu matka on {auto1.kuljettu_matka:d} "
       f"kilometriä.")
-
