98ddfdf4a05c45366de7045678ca85aea2b25a3e
tuukka <tuukkauu@metropolia.fi>
2024-10-23 14:49:57 +0300

    Tehtävä 9.4 kesken

--- Moduuli 9/9.4.py
+++ Moduuli 9/9.4.py
@@ -1,38 +1,52 @@
 import random
 
+autot = []
+
 class Auto():
-    def __init__(self, rekisteritunnus, huippunopeus, tamanhetkinen_nopeus):
+    def __init__(self, rekisteritunnus, huippunopeus):
         self.rekisteritunnus = rekisteritunnus
         self.huippunopeus = huippunopeus
-        self.tamanhetkinen_nopeus = tamanhetkinen_nopeus
+        self.tamanhetkinen_nopeus = 0
         self.kuljettu_matka = 0
 
     #kiihdytysmetodi
     def kiihdyta(self, muutos):
         uusi_nopeus = self.tamanhetkinen_nopeus + muutos
-
         if uusi_nopeus > self.huippunopeus:
             self.tamanhetkinen_nopeus = self.huippunopeus
-
         elif uusi_nopeus < 0:
             self.tamanhetkinen_nopeus = 0
-
         else:
             self.tamanhetkinen_nopeus = uusi_nopeus
 
     def kulje(self, tuntimaara):
         self.kuljettu_matka = self.kuljettu_matka + self.tamanhetkinen_nopeus * tuntimaara
 
-
-autot = []
-
 for a in range(10):
-    autot.append(f"auto {1 + a}")
+    huippunopeus = random.randint(100, 200)
+    rekisteritunnus = f"ABC-{a + 1}"
+    uusi_auto = Auto(rekisteritunnus, huippunopeus)
+    autot.append(uusi_auto)
 
-print(autot)
 
-for a in range(10):
-    random.randint()
+kilpailu_jatkuu = True
 
-# auto1 = Auto("ABC-123", 142,)
+while kilpailu_jatkuu:
+    for auto in autot:
+        # nop. muutos
+        nopeuden_muutos = random.randint(-10, 15)
+        auto.kiihdyta(nopeuden_muutos)
 
+        # 1 h
+        auto.kulje(1)
+
+        # 10k km
+        if auto.kuljettu_matka == 10000:
+            kilpailu_jatkuu = False
+            break
+
+# Tulostetaan kaikkien autojen tiedot
+print(f"{'Rekisteritunnus':<10}, {'Huippunopeus':<15}, {'Nopeus':<10}, {'Kuljettu matka'}")
+for auto in autot:
+    print(f"{auto.rekisteritunnus:<10}           {auto.huippunopeus:<15}"
+          f"{auto.tamanhetkinen_nopeus:<10}    {auto.kuljettu_matka:.2f}")
