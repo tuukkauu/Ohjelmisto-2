083e1f4ed1b7014584b3feddb7836bc4b20171be
tuukka <tuukkauu@metropolia.fi>
2024-10-24 22:00:59 +0300

    Tehtävä 10.3 kesken

---
+++ Moduuli 10/10.3.py
@@ -0,0 +1,82 @@
+
+class Hissi():
+    def __init__(self, alin = 1, ylin = 10):
+        self.alin = alin
+        self.ylin = ylin
+        self.nykyinen_kerros = alin
+
+    def siirry_kerrokseen(self, kerros):
+        if kerros < self.alin:
+            print("Alin mahdollinen kerros on ensimmäinen kerros.")
+            kerros = self.alin
+        elif kerros > self.ylin:
+            print("Ylin mahdollinen kerros on viides kerros.")
+            kerros = self.ylin
+
+        if self.nykyinen_kerros == kerros:
+            print(f"Hissi on nyt kerroksessa: {self.nykyinen_kerros}")
+            return
+
+        while self.nykyinen_kerros < kerros:
+            self.kerros_ylos()
+        while self.nykyinen_kerros > kerros:
+            self.kerros_alas()
+        else:
+            print(f"Olet nyt kerroksessa {self.nykyinen_kerros}")
+
+    def kerros_ylos(self):
+        if self.nykyinen_kerros < self.ylin:
+            self.nykyinen_kerros = self.nykyinen_kerros + 1
+            print(f"Hissi on nyt kerroksessa: {self.nykyinen_kerros}")
+        else:
+            print("Hissi on jo ylimmässä kerroksessa.")
+
+    def kerros_alas(self):
+        if self.nykyinen_kerros < self.alin:
+            self.nykyinen_kerros = self.nykyinen_kerros - 1
+            print(f"Hissi on nyt kerroksessa: {self.nykyinen_kerros}")
+        else:
+            print("Hissi on jo alimmassa kerroksessa.")
+
+
+# Talo class
+
+class Talo():
+    def __init__(self, alin = 1, ylin = 10, lukumaara = 3):
+        self.alin = alin
+        self.ylin = ylin
+        self.lukumaara = lukumaara
+        self.hissit = [Hissi(alin, ylin) for _ in range(lukumaara)]
+
+    def hissien_maara(self):
+        for h in range(self.lukumaara):
+            hissi = Hissi(self.ylin, self.alin)
+            self.hissit.append(hissi)
+            self.hissi = self.hissi + 1
+            print(h)
+            print(self.hissit)
+
+    def aja_hissia(self, hissin_numero, kohdekerros):
+        if 0 < hissin_numero < self.lukumaara:
+            print(f"Ajetaan {hissin_numero}. hissiä")
+            self.hissit[hissin_numero].siirry_kerrokseen(kohdekerros)
+        else:
+            print("Hissejä on vain kolme kappaletta.")
+
+    def palohalytys(self):
+        for p in range(self.hissit):
+            if self.nykyinen_kerros > self.alin:
+                self.siirry_kerrokseen = self.alin
+
+        """
+        if self.palohalytys:
+            self.palaa = self.alin
+            self.hissit[1, 2, 3].siirry_kerrokseen(self.alin)
+        """
+
+# Pääohjelma
+
+talo = Talo(1, 10, 3)
+
+talo.aja_hissia(1, 5)
+talo.palohalytys()
