a1b871e8aa5461b106454f7468a10f8aa43f279e
tuukka <tuukkauu@metropolia.fi>
2024-10-24 11:57:20 +0300

    Tehtävä 10.2 kesken

---
+++ Moduuli 10/10.2.py
@@ -0,0 +1,61 @@
+
+class Hissi():
+    def __init__(self, alin = 1, ylin = 10):
+        self.alin = alin
+        self.ylin = ylin
+        self.nykyinen_kerros = alin
+
+    def siirry_kerrokseen(self, kerros):
+        if kerros < self.alin:
+            print("Alin mahdollinen kerros on ensimmäinen kerros.")
+            kerros = self.alin
+        elif kerros > self.ylin:
+            print("Ylin mahdollinen kerros on viides kerros.")
+            kerros = self.ylin
+
+        if self.nykyinen_kerros == kerros:
+            print(f"Hissi on nyt kerroksessa: {self.nykyinen_kerros}")
+            return
+
+
+        while self.nykyinen_kerros < kerros:
+            self.kerros_ylos()
+        while self.nykyinen_kerros > kerros:
+            self.kerros_alas()
+        else:
+            print(f"Olet nyt kerroksessa {self.nykyinen_kerros}")
+
+    def kerros_ylos(self):
+        if self.nykyinen_kerros < self.ylin:
+            self.nykyinen_kerros = self.nykyinen_kerros + 1
+            print(f"Hissi on nyt kerroksessa: {self.nykyinen_kerros}")
+        else:
+            print("Hissi on jo ylimmässä kerroksessa.")
+
+    def kerros_alas(self):
+        if self.nykyinen_kerros < self.alin:
+            self.nykyinen_kerros = self.nykyinen_kerros - 1
+            print(f"Hissi on nyt kerroksessa: {self.nykyinen_kerros}")
+        else:
+            print("Hissi on jo alimmassa kerroksessa.")
+
+
+class Talo():
+    def __init__(self, alin = 1, ylin = 10, lukumaara = 3):
+        self.alin = alin
+        self.ylin = ylin
+        self.lukumaara = lukumaara
+
+    def hissien_maara(self):
+        hissit = []
+        for i in range(self.lukumaara):
+            hissi = hissit.append(Hissi(self.alin, self.ylin))
+            print(hissit)
+
+
+"""
+# Pääohjelmassa, kutsutaan hissi
+h = Hissi(1, 10)
+h.siirry_kerrokseen(-1)
+h.siirry_kerrokseen(5)
+"""
