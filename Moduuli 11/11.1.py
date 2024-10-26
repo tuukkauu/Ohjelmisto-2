ed16f68c1cb7aabdbcb8cb6bd5bb26cbabef331c
tuukka <tuukkauu@metropolia.fi>
2024-10-26 11:43:33 +0300

    Tehtävä 11.1 kesken

--- Moduuli 11/11.1.py
+++ Moduuli 11/11.1.py
@@ -1,1 +1,9 @@
 class Julkaisu():
+
+    def __init__(self, julkaisun_nimi, etunimi, sukunimi):
+        self.julkaisun_nimi = julkaisun_nimi
+        self.etunimi = etunimi
+        self.sukunimi = sukunimi
+
+    def tulosta_tiedot(self):
+        print(f"{self.julkaisun_nimi}, ")
