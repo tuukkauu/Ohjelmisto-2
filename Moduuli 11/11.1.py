71e759db8a6f887191a2bc3d82f703f5c9f2101f
tuukka <tuukkauu@metropolia.fi>
2024-10-28 17:30:05 +0200

    Tehtävä 11.1 ratkaistuna

--- Moduuli 11/11.1.py
+++ Moduuli 11/11.1.py
@@ -4,48 +4,38 @@ class Julkaisu():
         self.teos = teos
 
     def tulosta_tiedot(self):
-        print(f"{self.teos}")
+        print(f"{self.teos}", end=" ")
 
 
-class Kirjailija(Julkaisu):
-    def __init__(self, teos, kirjailija, etunimi, sukunimi):
+class Kirja(Julkaisu):
+    def __init__(self, teos, kirjailija, sivut):
         self.kirjailija = kirjailija
-        self.etunimi = etunimi
-        self.sukunimi = sukunimi
+        self.sivut = sivut
         super().__init__(teos)
 
     def tulosta_tiedot(self):
         super().tulosta_tiedot()
-        print(f"{self.kirjailija} {self.etunimi} {self.sukunimi}")
+        print(f"(Kirjailija: {self.kirjailija}, {self.sivut} sivua)", end=" ")
 
-class Paatoimittaja(Julkaisu):
-    def __init__(self, teos, paatoimittaja, etunimi, sukunimi):
+class Lehti(Julkaisu):
+    def __init__(self, teos, paatoimittaja):
         self.paatoimittaja = paatoimittaja
-        self.etunimi = etunimi
-        self.sukunimi = sukunimi
         super().__init__(teos)
 
     def tulosta_tiedot(self):
         super().tulosta_tiedot()
-        print(f"{self.paatoimittaja} {self.etunimi} {self.sukunimi}")
-
-
-"""
-class Julkaisija(Julkaisu):
-
-    def __init__(self, teos, ammattinimike, etunimi, sukunimi):
-        self.ammattinimike = ammattinimike
-        self.etunimi = etunimi
-        self.sukunimi = sukunimi
-        super().__init__(teos)
-
-
-    def tulosta_tiedot(self):
-        super().tulosta.tiedot()
-        print(f": {self.ammattinimike}, {self.etunimi} {self.sukunimi}")
-"""
+        print(f"(Päätoimittaja: {self.paatoimittaja})")
 
 
 # Pääohjelma
 
-Julkaisu.tulosta_tiedot()
+lista = []
+
+lista.append(Lehti("Aku Ankka", "Aki Hyyppä"))
+lista.append(Kirja("Hytti n:o 6", "Rosa Liksom", "200"))
+
+
+for i in lista:
+    i.tulosta_tiedot()
+
+
