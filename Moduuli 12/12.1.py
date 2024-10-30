6bbab2e6f4cbd612609a8b530870d5f9dfc92d49
tuukka <tuukkauu@metropolia.fi>
2024-10-30 10:20:05 +0200

    Tehtävä 12.1 ratkaistuna

--- Moduuli 12/12.1.py
+++ Moduuli 12/12.1.py
@@ -1,11 +1,10 @@
 import json
 import requests
-"
-k = input("Vastaa k, jos haluat kuulla Chuck Norris vitsin: ")
 
-vastaus = k
 
 # pyyntö:
-if vastaus == k:
-    pyyntö = "https://api.chucknorris.io/jokes/random/search/shows?q=:query"
+pyyntö = "https://api.chucknorris.io/jokes/random"
 
+vastaus = requests.get(pyyntö).json()
+
+print(vastaus["value"])
