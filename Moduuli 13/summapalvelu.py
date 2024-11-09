dbd55ad1f0fb9c18e92d1af37af2d5a04c07d7d5
tuukka <tuukkauu@metropolia.fi>
2024-11-09 21:45:36 +0200

    Tehtävä 13 moduulin harjoittelua

---
+++ Moduuli 13/summapalvelu.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+@app.route('/summa')
+def summa():
+    args = request.args
+    luku1 = float(args.get('luku1'))
+    luku2 = float(args.get('luku2'))
+    summa = luku1 + luku2
+    return str(summa)
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='localhost', port=3000)
