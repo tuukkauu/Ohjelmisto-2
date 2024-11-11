a3b5f2c96175ad8d40d2328d8fa73fcb1c24bafe
tuukka <tuukkauu@metropolia.fi>
2024-11-11 16:35:33 +0200

    Moduulin 13 harjoittelua

--- Moduuli 13/summapalvelu.py
+++ Moduuli 13/summapalvelu.py
@@ -7,7 +7,13 @@ def summa():
     luku1 = float(args.get('luku1'))
     luku2 = float(args.get('luku2'))
     summa = luku1 + luku2
-    return str(summa)
+
+    vastaus = {
+        "luku1": luku1,
+        "luku2": luku2,
+        "summa": summa
+    }
+    return vastaus
 
 if __name__ == '__main__':
-    app.run(use_reloader=True, host='localhost', port=3000)
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
